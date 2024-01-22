9dfd3106dd5b904e898295051ea39e58cc914156
wyf9 <wyf01239@163.com>
2024-01-22 12:55:17 +0800

    add import:css

--- js/import.js
+++ js/import.js
@@ -1,8 +1,19 @@
+// import JavaScript
 function wimport(base, files) {
   files.forEach(function (file) {
     var script = document.createElement("script");
     script.src = base + file;
     document.head.appendChild(script);
+  });
+}
+
+// import CSS
+function wimpcss(base, files) {
+  files.forEach(function (file) {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = base + file;
+    document.head.appendChild(link);
   });
 }
 
