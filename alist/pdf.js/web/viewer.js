73d5f45511d44389755828773fd5997c5254c55d
wyf9 <wyf9@wyf9.top>
2024-03-29 21:57:31 +0800

    ..

--- alist/pdf.js/web/viewer.js
+++ alist/pdf.js/web/viewer.js
@@ -3402,7 +3402,7 @@ const PDFViewerApplication = {
 exports.PDFViewerApplication = PDFViewerApplication;
 let validateFileURL;
 {
-  const HOSTED_VIEWER_ORIGINS = ["null", "http://alist-org.github.io", "https://alist-org.github.io", "https://alist.nn.ci"];
+  const HOSTED_VIEWER_ORIGINS = ["null", "http://alist-org.github.io", "https://alist-org.github.io", "https://alist.nn.ci", "https://ghsrc.wyf9.top"];
 
   validateFileURL = function (file) {
     if (!file) {
